4a74fa564e50cc62e2f4047466e3e495ed81969e
Sergio Urbina <checor@gmail.com>
2022-01-19 18:24:07 -0600

    Update biblioteca.sql

--- Sesion-02/Ejemplo-05/sql/biblioteca.sql
+++ Sesion-02/Ejemplo-05/sql/biblioteca.sql
@@ -1,4 +1,6 @@
 DROP DATABASE IF EXISTS Biblioteca;
 CREATE DATABASE Biblioteca;
-GRANT ALL PRIVILEGES ON Biblioteca.* TO Biblioteca@'%' IDENTIFIED BY 'Biblioteca';
-GRANT ALL PRIVILEGES ON Biblioteca.* TO Biblioteca@'localhost' IDENTIFIED BY 'Biblioteca';
+CREATE USER IF NOT EXISTS 'Biblioteca'@'localhost' IDENTIFIED BY 'Biblioteca';
+CREATE USER IF NOT EXISTS 'Biblioteca'@'%' IDENTIFIED BY 'Biblioteca';
+GRANT ALL PRIVILEGES ON Biblioteca.* TO Biblioteca@'%';
+GRANT ALL PRIVILEGES ON Biblioteca.* TO Biblioteca@'localhost';
